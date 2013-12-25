73127803439dfac96f00e6ef860d3b2f96259c6d
ilapshyn <igorko.lapshin@gmail.com>
2013-12-26 01:37:02 +0200

    KS: add some image handling

--- module/models.py
+++ module/models.py
@@ -5,7 +5,9 @@ DATE_PUBLISHED = 'date published'
 # Create your models here.
 class Divan(models.Model):
     name        = models.CharField(max_length=50)
+    image       = models.ImageField(upload_to = 'module/static/images/')
     pub_date    = models.DateField(DATE_PUBLISHED)
+    
     
     def __unicode__(self):
         return self.name
