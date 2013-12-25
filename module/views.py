877bd34a4bfaf06460bea843920dc7e1654bbf07
ilapshyn <igorko.lapshin@gmail.com>
2013-12-26 00:58:22 +0200

    KS: add some base styles

--- module/views.py
+++ module/views.py
@@ -1,7 +1,5 @@
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render_to_response
+
 
 def index(request):
-    template = loader.get_template('ks/index.html')
-    context = RequestContext(request, {})
-    return HttpResponse(template.render(context))
+    return render_to_response('ks/index.html',{})
