ce155d85e94f4b821a27f9ef34302ca8b63ef748
ilapshyn <igorko.lapshin@gmail.com>
2013-12-28 17:44:59 +0200

    KS: add more images

--- module/urls.py
+++ module/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import patterns, url
-
-from module import views
+import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^soft_mebli/', views.soft_mebli, name='soft_mebli'),
+    url(r'^$',              views.index,        name='index'),
+    url(r'^soft-mebel/',    views.soft_mebli,   name='soft-mebel'),
+    url(r'^vorota/',        views.vorota,       name='vorota'),
+    url(r'^kyhni/',         views.kyhni,        name='kyhni'),
+    url(r'^shafy-kupe/',    views.shafy_kupe,   name='shafy-kupe'),
+    url(r'^stoly-krisla/',  views.stoly_krisla, name='stoly-krisla'),
+    url(r'^spalni/',        views.spalni,       name='spalni'),
+    url(r'^shaluzi/',       views.shaluzi,      name='shaluzi'),
+    url(r'^kartyny/',       views.kartyny,      name='kartyny'),
 )
--- module/views.py
+++ module/views.py
@@ -8,3 +8,25 @@ def index(request):
 def soft_mebli(request):
     divans_list = Divan.objects.all();
     return render_to_response('ks/soft_mebli.html', {'divans' : divans_list})
+
+def vorota(request):
+    return render_to_response('ks/vorota.html', {})
+
+def kyhni(request):
+    return render_to_response('ks/kyhni.html', {})
+
+def shafy_kupe(request):
+    return render_to_response('ks/shafy-kupe.html', {})
+
+def stoly_krisla(request):
+    return render_to_response('ks/stoly-krisla.html', {})
+
+def spalni(request):
+    return render_to_response('ks/spalni.html', {})
+
+def shaluzi(request):
+    return render_to_response('ks/shaluzi.html', {})
+
+def kartyny(request):
+    return render_to_response('ks/kartyny.html', {})
+
